8489f457e2d6ebd92f15fce9d6b97c880ad2d6f8
clharge <clharge@users.noreply.github.com>
2016-08-23 15:25:48 +0200

    Update Spark's launch

--- oz-energy/src/main/java/org/ozwillo/energy/OzEnergyApplication.java
+++ oz-energy/src/main/java/org/ozwillo/energy/OzEnergyApplication.java
@@ -2,8 +2,10 @@ package org.ozwillo.energy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class OzEnergyApplication {
 
 	public static void main(String[] args) {
--- oz-energy/src/main/java/org/ozwillo/energy/spark/EnergyAggregationServices.java
+++ oz-energy/src/main/java/org/ozwillo/energy/spark/EnergyAggregationServices.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.commons.io.IOUtils;
 import org.apache.spark.launcher.SparkLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 
 public class EnergyAggregationServices {
 
@@ -20,19 +23,23 @@ public class EnergyAggregationServices {
 	}
 	
 	@Autowired
+	@PostConstruct
+	@Scheduled(cron="0 0 0 1/1 * ? *") //Runs at midnight every day every month
 	public void runAggregation() throws IOException, InterruptedException{
 		Map env = new HashMap<String,String>();
 		env.put("JAVA_HOME", System.getProperty("java.home"));
 		Process spark = new SparkLauncher(env)
 				/* Normally, you should set JAVA_HOME the following way, but it doesn't work 
-				 * See http://apache-spark-developers-list.1001551.n3.nabble.com/SparkLauncher-setJavaHome-does-not-set-JAVA-HOME-in-child-process-td14848.html
+				 * See http://apache-spark-developers-list.1001551.n3.nabble.com/
+				 * SparkLauncher-setJavaHome-does-not-set-JAVA-HOME-in-child-process-td14848.html
+				 * 
 				 * Solved by setting environment
 				 */
 				//.setJavaHome("/home/charge/installations/jdk1.8.0_91")
 			    .setSparkHome("/home/charge/installations/spark-1.6.1-bin-hadoop2.6")
 			    .setAppResource(mavenRepository + 
-			    		"/average-by-day-and-customer-key/average-by-day-and-customer-key_2.10/1.0/average-by-day-and-customer-key_2.10-1.0-assembly.jar")
-			    .setMainClass("AvgByDayAndCK").launch();
+			    		"/oz-energy-aggregations/oz-energy-aggregations_2.10/1.0/oz-energy-aggregations_2.10-1.0-assembly.jar")
+			    .setMainClass("Aggregations").addAppArgs("all").launch();
 
 		System.out.println("Waiting for finish...");
 		int exitCode = spark.waitFor();
