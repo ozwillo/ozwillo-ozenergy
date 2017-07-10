38118adb9945ff25217660e7919ef858f198ec1a
xia0ben <benoit.renault@smile.fr>
2017-07-10 15:42:13 +0200

    Move docker files to occiware-ozwillo repository

--- undeploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Uninstall unnecessary software
-apt-get purge -y vim \
-   && apt-get autoremove -y
-
-# Change Tomcat's default port from 8080 to 8088
-sed -i 's/8088/8080/g' /usr/local/tomcat/conf/server.xml
